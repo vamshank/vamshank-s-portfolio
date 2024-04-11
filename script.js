02a8a79b3f270121b3ce44372c73d70d6ffb5f00
Vamshank Reddygani <54182361+vamshank@users.noreply.github.com>
2024-04-11 14:05:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let sections =document.querySelectorAll('section');
+let navLinks =document.querySelectorAll('header nav a');
+
+windows.onscroll=() => {
+    sections.forEach(sec =>{
+        let top=window.scrollY;
+        let offset =sec.offseTop -150;
+        let height =sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+
+        if(top> offset && top<offset+height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*'+id+']'.classList.add('active'));
+            })
+        }
+    })
+}
